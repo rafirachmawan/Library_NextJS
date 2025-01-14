e98dd908251eaae23f42e1e87fdf040a698e80bf
rafirachmawan <rafirachmawan1987@gmail.com>
2025-01-14 22:05:54 +0700

    Update BookOverview

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 
 const Home = () => (
   <>
-    <Button>Click Me </Button>
+    <BookOverview />
+    <BookList />
   </>
 );
 export default Home;
