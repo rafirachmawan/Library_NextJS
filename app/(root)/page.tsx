5d22aa747791fb9274927ef2d7294fb56855b74f
rafirachmawan <rafirachmawan1987@gmail.com>
2025-01-14 22:54:10 +0700

    Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constans/index";
 
 const Home = () => (
   <>
-    <BookOverview />
-    <BookList />
+    <BookOverview {...sampleBooks[0]} />
+    <BookList
+      title="Latest Books"
+      Book={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 export default Home;
--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,10 +1,36 @@
 import React from "react";
+import Image from "@/node_modules/next/image";
 
-const BookOverview = () => {
+const BookOverview = ({
+  title,
+  author,
+  genre,
+  rating,
+  total_copies,
+  available_copies,
+  description,
+  color,
+  cover,
+}: Book) => {
   return (
-    <section className="BookOverview">
+    <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
-        <h1>A Long Book Title</h1>
+        <h1>{title}</h1>
+
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+          <p>
+            Category{" "}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+
+          <div className="flex flex-row gap-1">
+            <Image src="./icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
+        </div>
       </div>
     </section>
   );
