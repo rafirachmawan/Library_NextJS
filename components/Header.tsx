dee9e8c1ef6636b693954852279a87f2340c74ca
rafirachmawan <rafirachmawan1987@gmail.com>
2025-01-14 20:22:48 +0700

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,27 @@
 import Link from "@/node_modules/next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const Header = () => {
   const pathname = usePathname();
 
   return (
     <header className="my-10 flex justfiy-between gap-5">
-      <Link href="/">BookWise</Link>
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
 
       <ul className="flex flex-row items-center gap-8">
         <li>
-          <Link href="/library" className="text-base cursor-pointer capitalize">
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
             Library
           </Link>
         </li>
