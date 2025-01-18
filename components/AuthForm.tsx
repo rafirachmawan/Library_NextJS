d5ef384472395096a4276758754f6cd2aaa953ee
rafirachmawan <rafirachmawan1987@gmail.com>
2025-01-18 14:05:46 +0700

    Update Auth Form Sign Up And Sign In

---
+++ components/AuthForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props
+
+const AuthForm = ({ type, schema, defaultValues,OnSubmit}: Props) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
